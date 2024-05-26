135cbb9325991434ee70695262f31b7f2424c24a
Shikhar0895 <shikhar0895@gmail.com>
2024-05-26 18:53:42 +0530

    initial commit

--- src/shared/widgets/header/Header.tsx
+++ src/shared/widgets/header/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="w-full sticky top-0 left-0 z-[999] border-b border-b-[#000] px-10 flex items-center"></header>
+    <header className="w-full sticky top-0 left-0 z-[999] border-b border-b-[#000] px-10 flex "></header>
   );
 };
 
