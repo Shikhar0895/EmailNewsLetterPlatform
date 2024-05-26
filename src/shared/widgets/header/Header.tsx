617e304433466ffab6a480e62bd99da4a2de6fe7
Shikhar0895 <shikhar0895@gmail.com>
2024-05-26 17:21:22 +0530

    Add existing project files to the push to github

---
+++ src/shared/widgets/header/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="w-full sticky top-0 left-0 z-[999] border-b border-b-[#000] px-10 flex items-center"></header>
+  );
+};
+
+export default Header;
