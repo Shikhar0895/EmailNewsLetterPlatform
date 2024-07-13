51504a19397f3e6ec32c47706d8265760a2a65be
Shikhar0895 <shikhar0895@gmail.com>
2024-07-13 17:15:13 +0530

    fixed type error

--- src/app/shared/components/tabs/settings.tabs.tsx
+++ src/app/shared/components/tabs/settings.tabs.tsx
@@ -9,7 +9,7 @@ const SettingsTabs = () => {
       variant={"underlined"}
       aria-label="Tabs variants"
       selectedKey={activeItem}
-      onSelectionChange={setActiveItem}
+      onSelectionChange={() => setActiveItem}
     >
       <Tab key="API Access" title="API Access" />
       <Tab key="Customize Profile" title="Customize Profile" />
