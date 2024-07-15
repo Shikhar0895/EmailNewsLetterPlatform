110eb86f8439e943b92cb3b3f86a3bed96b2166d
Shikhar0895 <shikhar0895@gmail.com>
2024-07-15 23:11:13 +0530

    type error

--- src/app/shared/components/tabs/settings.tabs.tsx
+++ src/app/shared/components/tabs/settings.tabs.tsx
@@ -1,15 +1,15 @@
 import useSettingsFilter from "../../hooks/useSettingFilter";
-import { Tabs, Tab } from "@nextui-org/react";
+import { Tabs, Tab, TabsProps } from "@nextui-org/react";
 
 const SettingsTabs = () => {
   const { activeItem, setActiveItem } = useSettingsFilter();
-  console.log(activeItem, setActiveItem);
+
   return (
     <Tabs
       variant={"underlined"}
       aria-label="Tabs variants"
       selectedKey={activeItem}
-      onSelectionChange={setActiveItem}
+      onSelectionChange={setActiveItem as any}
     >
       <Tab key="API Access" title="API Access" />
       <Tab key="Customize Profile" title="Customize Profile" />
